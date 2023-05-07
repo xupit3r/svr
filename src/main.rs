4947b76645b8263c096d6a66ce113196000c22c0
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-07 08:07:28 -0400

    put some basic control flow and error handling in

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,36 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-    let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    match fs::read_to_string(filename) {
+        Ok(contents) => {
+            let length = contents.len();
+            let response =
+                format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+            match stream.write_all(response.as_bytes()) {
+                Ok(_o) => println!("success response sent"),
+                Err(_e) => println!("FUCKING 'LL!")
+            }
+        },
+        Err(_e) => {
+            let message = "sumfing bad happend 😭";
+            let length = message.len();
+            let response = 
+                format!("HTTP/1.1 500 SERVER ERROR\r\nContent-Length: {length}\r\n\r\n{message}");
+
+            match stream.write_all(response.as_bytes()) {
+                Ok(_o) => println!("error response sent."),
+                Err(_e) => println!("FUCK!")
+            }
+        }
+    }
 }
