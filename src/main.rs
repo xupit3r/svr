28b79d1dd9cf71135d9d4757a3dbed8708cc0d2f
Joe D'Alessandro <joe@thejoeshow.net>
2023-05-13 06:31:26 -0400

    going to check in this none working stuff  * just don't want to lose it...  * think it will be better to start with a bit more of a foundation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::{
     fs,
+    collections::HashMap,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
+
+use  http::{Request, Response, StatusCode}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -20,33 +23,65 @@ fn handle_connection(mut stream: TcpStream) {
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "templates/hello.html")
+        ("200 OK", "templates/hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "templates/404.html")
+        ("404 NOT FOUND", "templates/404.html")
     };
 
 
     match fs::read_to_string(filename) {
         Ok(contents) => {
-            let length = contents.len();
-            let response =
-                format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+            let response = create_response(status, contents.as_str());
 
             match stream.write_all(response.as_bytes()) {
-                Ok(_o) => println!("success response sent"),
+                Ok(_) => println!("success response sent"),
                 Err(_e) => println!("FUCKING 'LL!")
             }
         },
         Err(_e) => {
+            let status = "500 SERVER ERROR";
             let message = "sumfing bad happend 😭";
-            let length = message.len();
-            let response = 
-                format!("HTTP/1.1 500 SERVER ERROR\r\nContent-Length: {length}\r\n\r\n{message}");
+            let response = create_response(status, message);
 
             match stream.write_all(response.as_bytes()) {
-                Ok(_o) => println!("error response sent."),
+                Ok(_) => println!("error response sent."),
                 Err(_e) => println!("FUCK!")
             }
         }
+    }
+}
+
+fn parse_request(mut stream: TcpStream) -> Result<Request, io::Error> {
+    let buf_reader = BufReader::new(&mut stream);
+    let method_uri = buf_reader.lines().next().unwrap();
+
+    match method_uri {
+        Ok(preamble) => {
+            let headers = buf_reader.lines()
+        },
+        Err(e) => {
+            respond(stream, StatusCode::BAD_REQUEST, "Malformed");
+        }
+    }
+    let request_lines: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+}
+
+fn create_response(status: StatusCode, contents: &str) -> Response<()> {
+    Response::buider()
+        .status(status)
+        .body(contents)
+}
+
+fn respond(mut stream: TcpStream, status: &str, contents: &str) {
+    let response = create_response(status, contents);
+    let bx = response.unwrap();
+
+    match stream.write_all(response.as_bytes()) {
+        Ok(_) => println!("response sent."),
+        Err(e) => println!("fuck...response not sent {:?}", e)
     }
 }
